b4b7a6c0b19ab804b54b70597d0c86c4c7854767
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2019-10-03 11:35:09 +0200

    MAJ allumettes.c
    Ajouté la saisie des noms des joueurs.

--- allumettes.c
+++ allumettes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-winner (int a, int j)
+int winner (int a, int j)
 {
        int win = 0;
        if (a>4)
@@ -24,7 +24,14 @@ winner (int a, int j)
             
 int main()
 {
-    printf("### JEU DES ALLUMETTES ###\n\n Il faut deux joueurs.\n\n### REGLES ###\n\nIl y a 20 allumettes au depart. Chaque joueur prend tour à tour entre 1 et 3 allumettes. Le joueur qui prend la derniere allumette perd la partie. Le joueur 1 commence.\n\n### Place au jeu! ###\n\nIndiquez le nombre d'allumlettes que vous tirez.\n");
+    printf("### JEU DES ALLUMETTES ###\n\n Il faut deux joueurs.\n\n### REGLES ###\n\n - Il y a 20 allumettes au depart.\n - Chaque joueur prend tour à tour entre 1 et 3 allumettes.\n - Le joueur qui prend la derniere allumette perd la partie.\n - Le joueur 1 commence.\n\n### Place au jeu! ###\n");
+    printf("Le joueur 1 s'appelle: ");
+    char nom1[15];
+    scanf("%s", &nom1[0]);
+    printf("\n\nLe joueur 2 s'appelle: ");
+    char nom2[0];
+    scanf("%s", &nom2[0]);
+    printf("\n\nIndiquez le nombre d'allumlettes que vous tirez.\n");
     int a = 20;
     int x = 0;
     int j = 0;
@@ -32,38 +39,38 @@ int main()
     while (1==1)
     {
           j=1;
-          printf("joueur 1:");
+          printf("%s :", nom1 );
           scanf("%i", &x);
           a = a-x;
-          printf("il reste %i", a);
+          printf("\nIl reste %i allumettes.\n\n", a);
           win = winner(a, j);
           if (win == 1)
           {
-                  printf("Joueur 1 gagne");
+                  printf("%s a gagne!\n", nom1);
                   system("PAUSE");
                   return 0;
           }
           if (win == 2)
           {
-                  printf("Joueur 2 gagne");
+                  printf("%s a gagne!\n", nom2);
                   system("PAUSE");
                   return 0;
           }
           j=2;
-          printf("joueur 2:");
+          printf("%s: ", nom2);
           scanf("%i", &x);
           a = a-x;
-          printf("il reste %i", a);
+          printf(" \nIl reste %i allumettes.\n\n", a);
           win = winner(a, j);
           if (win == 1)
           {
-                  printf("Joueur 1 gagne");
+                  printf("%s a gagne!\n", nom1);
                   system("PAUSE");
                   return 0;
           }
           if (win == 2)
           {
-                  printf("Joueur 2 gagne");
+                  printf("%s a gagne!\n", nom2);
                   system("PAUSE");
                   return 0;
           }
