2e0da863dd3bc1365337d721fbd792593d9043d7
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2020-03-02 09:29:12 +0100

    Update paxion-exercice3.c

--- paxion-exercice3.c
+++ paxion-exercice3.c
@@ -3,16 +3,19 @@
 
 int main()
 {
-        printf("\tTP4 - Exercice 3\n\n");
+        printf("\n\tTP4 - Exercice 3\n\n");
         int tab[20] = {4,-2,-23,4,34,-67,8,9,-10,11,4,12,-53,19,11,-60,24,12,89,19};
-        int i;
+        int i = 0;
+        int j = 0;
         printf("\n");
         for(i=0; i<20; i++)
         {
                 printf("\t%d",tab[i]);
-                if((i == 4) | (i == 9) | (i == 14) | (i == 19))
+                j++;
+                if(j == 5)
                 {
                         printf("\n");
+                        j = 0;
                 }
         }
         printf("\n");
