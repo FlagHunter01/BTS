5998a8f6297cf415b4c70b4364e6d72e91e92875
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2020-10-12 12:25:25 +0200

    Update test.c

--- SN2/Paxion/test.c
+++ SN2/Paxion/test.c
@@ -25,38 +25,32 @@
 #include <stdio.h>
 #include <stdbool.h>   // bool
 #include <termios.h>   // speed_t, tcflag_t ...
-#include <unistd.h>    // access()
+#include <unistd.h>	   // access()
 #include <sys/types.h> //open()
 #include <sys/stat.h>  //open()
-#include <fcntl.h>     //open()
+#include <fcntl.h>	   //open()
 
 bool OpenPort();
-bool ConfigPort();
+void ConfigPort();
 bool ClosePort();
 
 int main()
 {
-    printf("\n\t########## FEU 1 ##########\n\n\n");
+	printf("\n\t########## FEU 1 ##########\n\n\n");
 
-    char file_path[] = "/dev/ttyUSBX";
-    printf("Ouverture\n");
-    int file_descriptor = open(file_path, O_RDWR); // On ouvre un descripteur
-    printf("file descriptor = %d\n", file_descriptor);
-    if (file_descriptor)
-    { // Si on a obtenu le descripteur
-        if (close(file_descriptor) == 0)
-        {
-            printf("Port ferme correctement.\n");
-        }
-        else
-        {
-            printf("Port mal ferme.\n");
-        }
-    }
-    else
-    { // Si on n'a pas obtenu le descripteur
-        printf("Echec lors de l'ouverture du port.\nFin du programme.\n\n\n");
-        return 0;
-    }
-    return 0;
+	char file_path[] = "/dev/ttyUSBX";
+
+	if (OpenPort())
+	{
+		ConfigPort();
+		if (ClosePort())
+		{
+			return EXIT_SUCCESS;
+		}
+		else
+		{
+			return EXIT_FAILURE;
+		}
+	}
+	return EXIT_FAILURE;
 }
