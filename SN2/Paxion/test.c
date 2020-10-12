4f91bc4e16751c9b5c6c42799a72d65ee317df94
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2020-10-12 11:47:08 +0200

    Create test.c

---
+++ SN2/Paxion/test.c
@@ -0,0 +1,54 @@
+/* TODO:
+ - Penser a faire des boucles qui relancent plusieurs fois les fonctions qui ont échoué
+ - Enregister l'ancienne configuration pour la restaurer à la fin du programme
+ - Si cedrtaines configurations ont échoué, essayer de continuer en failproof le programme de management du feu
+
+ CONFIG:
+ 	// control mode flags
+	newtio.c_cflag = BAUDRATE | CSTOPB | CREAD | CS8 | CLOCAL;
+	// input mode flags
+	newtio.c_iflag = IGNBRK;
+	// output mode flags
+	newtio.c_oflag = 0;
+	// local mode flags
+	newtio.c_lflag = 0;
+	// flush all data
+	if(tcflush(serfd, TCIOFLUSH)
+*/
+
+#include <stdio.h>
+#include <stdbool.h>   // bool
+#include <termios.h>   // speed_t, tcflag_t ...
+#include <unistd.h>    // access()
+#include <sys/types.h> //open()
+#include <sys/stat.h>  //open()
+#include <fcntl.h>     //open()
+
+int main()
+{
+    printf("\n\t############### TEST FEU 1 ###############\n\n\n");
+
+    /* On sort les variables suceptibles d'être changées au début
+    pour pouvoir les modifier facilement */
+    char file_path[] = "/dev/ttyUSB0";
+    printf("Ouverture\n");
+    int file_descriptor = open(file_path, O_RDWR); // On ouvre un descripteur
+    printf("file descriptor = %d\n", file_descriptor);
+    if (file_descriptor)
+    { // Si on a obtenu le descripteur
+        if (close(file_descriptor) == 0)
+        {
+            printf("Port ferme correctement.\n");
+        }
+        else
+        {
+            printf("Port mal ferme.\n");
+        }
+    }
+    else
+    { // Si on n'a pas obtenu le descripteur
+        printf("Echec lors de l'ouverture du port.\nFin du programme.\n\n\n");
+        return 0;
+    }
+    return 0;
+}
