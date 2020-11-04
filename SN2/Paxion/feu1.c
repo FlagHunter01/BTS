6b9c686cbdfa09bef020383f0cee4908f712dd7d
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2020-11-04 15:12:00 +0100

    Update feu1.c

--- SN2/Paxion/feu1.c
+++ SN2/Paxion/feu1.c
@@ -7,18 +7,18 @@ TODO:
 
 */
 
-#include <stdio.h> // perror()
+#include <stdio.h>     // perror()
 #include <stdbool.h>   // bool
 #include <termios.h>   // speed_t, tcflag_t ...
 #include <unistd.h>    // access()
 #include <sys/types.h> //open()
 #include <sys/stat.h>  //open()
 #include <fcntl.h>     //open()
-#include <stdlib.h> // exit()
+#include <stdlib.h>    // exit()
 
-int OpenPort(char *file_path);                                                   // Retourne le descripteur
+int OpenPort(char *file_path);                                 // Retourne le descripteur
 bool ConfigPort(int file_descriptor, struct termios *oldConf); // Configure la connexion
-bool ClosePort(int file_descriptor, struct termios *oldConf); // Restore l'ancienne configuration et ferme de descripteur
+bool ClosePort(int file_descriptor, struct termios *oldConf);  // Restore l'ancienne configuration et ferme de descripteur
 
 int main()
 {
@@ -36,14 +36,18 @@ int main()
 
     if (file_descriptor > 0)
     {
-        if (ConfigPort(file_descriptor, &oldConf)){
-            if(ClosePort(file_descriptor, &oldConf)){
+        if (ConfigPort(file_descriptor, &oldConf))
+        {
+            if (ClosePort(file_descriptor, &oldConf))
+            {
                 exit(EXIT_SUCCESS);
             }
             exit(EXIT_FAILURE);
         }
-        else{
-            if(ClosePort(file_descriptor, &oldConf)){
+        else
+        {
+            if (ClosePort(file_descriptor, &oldConf))
+            {
                 exit(EXIT_FAILURE);
             }
             exit(EXIT_FAILURE);
@@ -56,10 +60,11 @@ int main()
         exit(EXIT_FAILURE);
     }
     perror("\nCe message ne doit jamais s'afficher.");
-        exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
 
-int OpenPort(char *file_path){
+int OpenPort(char *file_path)
+{
     // Compteur
     char i = '2';
 
@@ -67,22 +72,25 @@ int OpenPort(char *file_path){
     int file_descriptor;
 
     printf("Ouverture du port ...\n");
-    for (; i >= '0'; i--){
+    for (; i >= '0'; i--)
+    {
         //file_path[sizeof(file_path) - 2] = i; // Pk sizeof(file_path) ne prend pas la taille du pointeur ?!
         file_path[11] = i;
         // Ouverture en lecture + ecriture. O_NOCTTY a comprendre.
-        file_descriptor = open(file_path, O_RDWR | O_NOCTTY );
-        if (file_descriptor > 0){
+        file_descriptor = open(file_path, O_RDWR | O_NOCTTY);
+        if (file_descriptor > 0)
+        {
             printf("\tOuverture réussie de %s.\n\n", file_path);
             return file_descriptor;
         }
-        else{
+        else
+        {
             printf("\tEchec lors de l'ouverture de %s.\n\n", file_path);
         }
     }
     perror("\tAucun port n'a pu être ouvert.");
     printf("\n\n");
-            return file_descriptor;
+    return file_descriptor;
 }
 
 bool ConfigPort(int file_descriptor, struct termios *oldConf)
@@ -93,57 +101,59 @@ bool ConfigPort(int file_descriptor, struct termios *oldConf)
         // Nouvelle configuration à soumettre
         struct termios newConf;
 
-    /* 
-    B9600: Vitesse de 9600 Bauds
-    CRTSCTS : output hardware flow control (?)
-    CS8     : 8 bit, pas de contrôle de parité, 1 bit de stop
-    CLOCAL  : ignore les lignes de contrôle de modem
-    CREAD   : lire les réponses
-    */
-    newConf.c_cflag = B9600 | CRTSCTS | CS8 | CLOCAL | CREAD;
-         
-    /*
-    IGNPAR  : ignorer les octets avec des erreurs de parité
-    ICRNL   : Transforme le retour chariot en "jnouvelle ligne"
-    */
-    newConf.c_iflag = IGNPAR | ICRNL;
-         
-    /*
-    "output mode". On le garde nul.
-    */
-    newConf.c_oflag = 0;
-         
-    /*
-    ICANON  : Quelque chose lié a la compatibilité majuscules / minuscules
-    */
-    newConf.c_lflag = ICANON;
+        /* 
+        B9600: Vitesse de 9600 Bauds
+        CRTSCTS : output hardware flow control (?)
+        CS8     : 8 bit, pas de contrôle de parité, 1 bit de stop
+        CLOCAL  : ignore les lignes de contrôle de modem
+        CREAD   : lire les réponses
+        */
+        newConf.c_cflag = B9600 | CRTSCTS | CS8 | CLOCAL | CREAD;
 
-    // Je ne suis pas sur qu'il faut garder ce qui suit     
-    newConf.c_cc[VINTR]    = 0;     /* Ctrl-c */ 
-    newConf.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
-    newConf.c_cc[VERASE]   = 0;     /* del */
-    newConf.c_cc[VKILL]    = 0;     /* @ */
-    newConf.c_cc[VEOF]     = 4;     /* Ctrl-d */
-    newConf.c_cc[VTIME]    = 0;     /* inter-character timer unused */
-    newConf.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
-    newConf.c_cc[VSWTC]    = 0;     /* '\0' */
-    newConf.c_cc[VSTART]   = 0;     /* Ctrl-q */ 
-    newConf.c_cc[VSTOP]    = 0;     /* Ctrl-s */
-    newConf.c_cc[VSUSP]    = 0;     /* Ctrl-z */
-    newConf.c_cc[VEOL]     = 0;     /* '\0' */
-    newConf.c_cc[VREPRINT] = 0;     /* Ctrl-r */
-    newConf.c_cc[VDISCARD] = 0;     /* Ctrl-u */
-    newConf.c_cc[VWERASE]  = 0;     /* Ctrl-w */
-    newConf.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
-    newConf.c_cc[VEOL2]    = 0;     /* '\0' */
+        /*
+        IGNPAR  : ignorer les octets avec des erreurs de parité
+        ICRNL   : Transforme le retour chariot en "jnouvelle ligne"
+        */
+        newConf.c_iflag = IGNPAR | ICRNL;
+
+        /*
+        "output mode". On le garde nul.
+        */
+        newConf.c_oflag = 0;
+
+        /*
+        ICANON  : Quelque chose lié a la compatibilité majuscules / minuscules
+        */
+        newConf.c_lflag = ICANON;
+
+        // Je ne suis pas sur qu'il faut garder ce qui suit
+        newConf.c_cc[VINTR] = 0;    /* Ctrl-c */
+        newConf.c_cc[VQUIT] = 0;    /* Ctrl-\ */
+        newConf.c_cc[VERASE] = 0;   /* del */
+        newConf.c_cc[VKILL] = 0;    /* @ */
+        newConf.c_cc[VEOF] = 4;     /* Ctrl-d */
+        newConf.c_cc[VTIME] = 0;    /* inter-character timer unused */
+        newConf.c_cc[VMIN] = 1;     /* blocking read until 1 character arrives */
+        newConf.c_cc[VSWTC] = 0;    /* '\0' */
+        newConf.c_cc[VSTART] = 0;   /* Ctrl-q */
+        newConf.c_cc[VSTOP] = 0;    /* Ctrl-s */
+        newConf.c_cc[VSUSP] = 0;    /* Ctrl-z */
+        newConf.c_cc[VEOL] = 0;     /* '\0' */
+        newConf.c_cc[VREPRINT] = 0; /* Ctrl-r */
+        newConf.c_cc[VDISCARD] = 0; /* Ctrl-u */
+        newConf.c_cc[VWERASE] = 0;  /* Ctrl-w */
+        newConf.c_cc[VLNEXT] = 0;   /* Ctrl-v */
+        newConf.c_cc[VEOL2] = 0;    /* '\0' */
 
         tcflush(file_descriptor, TCIFLUSH);
 
-        if(tcsetattr(file_descriptor, TCSANOW, &newConf) == 0){
+        if (tcsetattr(file_descriptor, TCSANOW, &newConf) == 0)
+        {
             printf("\tConfiguration réussie.\n\n");
             return true;
         }
-        else{
+        else
+        {
             perror("\tEchec lors de la configuration du port.");
             return false;
             printf("\n\n");
@@ -161,16 +171,19 @@ bool ConfigPort(int file_descriptor, struct termios *oldConf)
     return false;
 }
 
-bool ClosePort(int file_descriptor, struct termios *oldConf){
+bool ClosePort(int file_descriptor, struct termios *oldConf)
+{
     printf("Fermeture du port ...\n");
-    if (tcsetattr(file_descriptor,TCSANOW, oldConf) == 0){
+    if (tcsetattr(file_descriptor, TCSANOW, oldConf) == 0)
+    {
         printf("\tRestauration de la configuration initiale réussie.\n");
         if (close(file_descriptor) == 0)
         {
             printf("\tFermeture du descripteur réussie.\n\n");
             return true;
         }
-        else{
+        else
+        {
             perror("\tEchec lors de la fermeture du descripteur de fichiers.");
             return false;
             printf("\n\n");
@@ -178,7 +191,8 @@ bool ClosePort(int file_descriptor, struct termios *oldConf){
         perror("\tCe code ne doit jamais s'exécutrer.");
         return false;
     }
-    else{
+    else
+    {
         perror("\tEchec lors de la restauration de l'ancienne configuration.");
         printf("\n\n");
         if (close(file_descriptor) == 0)
@@ -186,10 +200,11 @@ bool ClosePort(int file_descriptor, struct termios *oldConf){
             printf("\tFermeture du descripteur réussie.\n\n");
             return false;
         }
-        else{
+        else
+        {
             perror("\tEchec lors de la fermeture du descripteur de fichiers.");
             return false;
             printf("\n\n");
-    }
+        }
     }
 }
