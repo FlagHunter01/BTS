2b8059d52ba9ef036d61644065aead7b1fd839ec
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2020-11-09 21:36:09 +0100

    Update feu5.c

--- SN2/Paxion/feu5.c
+++ SN2/Paxion/feu5.c
@@ -23,9 +23,9 @@ bool ConfigPort(int file_descriptor, struct termios *oldConf); // Configure la c
 bool ClosePort(int file_descriptor, struct termios *oldConf);  // Restore l'ancienne configuration et ferme de descripteur
 void Command(int file_descriptor);                             // Communique les commandes au feu
 void Send(int file_descriptor, char *command, char *answer);   // Sert a envoyer / verifier reception des commandes
-void Left(int file_descriptor, int intensity); // Commande l'ampoule gauche
-void Top(int file_descriptor, int intensity); // Commande l'ampoule du haut
-void Right(int file_descriptor, int intensity); // Commande l'ampoule droite
+void Left(int file_descriptor, int intensity);                 // Commande l'ampoule gauche
+void Top(int file_descriptor, int intensity);                  // Commande l'ampoule du haut
+void Right(int file_descriptor, int intensity);                // Commande l'ampoule droite
 
 int main()
 {
@@ -185,20 +185,31 @@ bool ClosePort(int file_descriptor, struct termios *oldConf)
 }
 
 void Command(int file_descriptor)
-{   int i;
+{
+    int i;
     int j = 0;
     int k = 127;
     int l = 254;
-    for (i=0;i<10;i++){
-    Left(file_descriptor, j);
-    Top(file_descriptor, k);
-    Right(file_descriptor, l);
-    j += 127;
-    k+=127;
-    l+=127;
-    if (j > 254){j=0;}
-    if (k > 254){k=0;}
-    if (l > 254){l=0;}
+    for (i = 0; i < 10; i++)
+    {
+        Left(file_descriptor, j);
+        Top(file_descriptor, k);
+        Right(file_descriptor, l);
+        j += 127;
+        k += 127;
+        l += 127;
+        if (j > 254)
+        {
+            j = 0;
+        }
+        if (k > 254)
+        {
+            k = 0;
+        }
+        if (l > 254)
+        {
+            l = 0;
+        }
     }
     Left(file_descriptor, 0);
     Top(file_descriptor, 0);
@@ -231,52 +242,74 @@ void Send(int file_descriptor, char *command, char *answer)
     }
 }
 
-void Left(int file_descriptor, int intensity){
+void Left(int file_descriptor, int intensity)
+{
     int i;
-char intensity_str[4];
+    char intensity_str[4];
     char command[24];
     char answer[24];
-    for (i=0;i<25;i++){command[i] = '\0';}for(i=0;i<25;i++){answer[i] = '\0';}
-    strcat(command, "l"); 
+    for (i = 0; i < 25; i++)
+    {
+        command[i] = '\0';
+    }
+    for (i = 0; i < 25; i++)
+    {
+        answer[i] = '\0';
+    }
+    strcat(command, "l");
     sprintf(intensity_str, "%d", intensity);
     strcat(command, intensity_str);
     strcat(command, "\n");
-    strcat(answer, "jar");   
+    strcat(answer, "jar");
     strcat(answer, intensity_str);
     strcat(answer, "\n");
     Send(file_descriptor, command, answer);
 }
 
-void Top(int file_descriptor, int intensity){
+void Top(int file_descriptor, int intensity)
+{
     int i;
-char intensity_str[4];
+    char intensity_str[4];
     char command[24];
     char answer[24];
-    for (i=0;i<25;i++){command[i] = '\0';}for(i=0;i<25;i++){answer[i] = '\0';}
-    strcat(command, "t"); 
+    for (i = 0; i < 25; i++)
+    {
+        command[i] = '\0';
+    }
+    for (i = 0; i < 25; i++)
+    {
+        answer[i] = '\0';
+    }
+    strcat(command, "t");
     sprintf(intensity_str, "%d", intensity);
     strcat(command, intensity_str);
     strcat(command, "\n");
-    strcat(answer, "top");   
+    strcat(answer, "top");
     strcat(answer, intensity_str);
     strcat(answer, "\n");
     Send(file_descriptor, command, answer);
-
 }
 
-void Right(int file_descriptor, int intensity){
+void Right(int file_descriptor, int intensity)
+{
     int i;
-char intensity_str[4];
+    char intensity_str[4];
     char command[24];
     char answer[24];
-    for (i=0;i<25;i++){command[i] = '\0';}for(i=0;i<25;i++){answer[i] = '\0';}
-    strcat(command, "r"); 
+    for (i = 0; i < 25; i++)
+    {
+        command[i] = '\0';
+    }
+    for (i = 0; i < 25; i++)
+    {
+        answer[i] = '\0';
+    }
+    strcat(command, "r");
     sprintf(intensity_str, "%d", intensity);
     strcat(command, intensity_str);
     strcat(command, "\n");
-    strcat(answer, "cour");   
+    strcat(answer, "cour");
     strcat(answer, intensity_str);
     strcat(answer, "\n");
     Send(file_descriptor, command, answer);
-
 }
